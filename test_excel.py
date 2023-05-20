3740ac6f1247b40abbaeb84320fd0c23fad959f2
Harutann <harutaikou02231127@gmail.com>
2023-05-20 16:46:57 +0900

    test

--- test_excel.py
+++ test_excel.py
@@ -10,4 +10,11 @@ ws.append([1, 2, 3])
 import datetime
 ws['A2'] = datetime.datetime.now()
 
+ws_new = wb.create_sheet("追加シート")
+value_data = 0
+for x in range(1, 11):
+    for y in range(1, 11):
+        value_data = 10 * (x - 1) + y
+        ws_new.cell(row=x, column=y).value = value_data
+
 wb.save("sample.xlsx")
