c15a008ffb2c25475902bccbf61a711bf2a652d6
Harutann <harutaikou02231127@gmail.com>
2023-05-20 17:32:22 +0900

    test

--- test_power.py
+++ test_power.py
@@ -10,3 +10,19 @@ title.text = "Hello, World!"
 subtitle.text = "python-pptx was here!"
 
 prs.save('test.pptx')
+
+title.text = "変更後のパワーポイントファイル"
+subtitle.text = "レイアウトを変更するとどうなる？"
+
+count = 0
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                count += len(run.text)
+
+print(f'パワーポイント内の文字数は{count}個です。')
+
+prs.save('test2.pptx')
