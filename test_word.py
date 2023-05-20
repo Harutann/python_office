21bdb839ecc84d40a51f8fcc95d487284a1ef9b2
Harutann <harutaikou02231127@gmail.com>
2023-05-20 17:31:40 +0900

    test

--- test_word.py
+++ test_word.py
@@ -6,3 +6,21 @@ document.add_heading('簡単なWordドキュメントのタイトル', 0)
 document.add_paragraph('簡単なWordドキュメントのテキスト')
 
 document.save('sample.docx')
+
+from docx import Document
+
+
+def main():
+    document = Document('sample.docx')
+    document.add_picture('sample.jpg')
+    count = 0
+    for data in document.paragraphs:
+        count += len(data.text)
+
+    print(f'sample.docxの中の文字数は{count}個です。')
+
+    document.save('sample_after.docx')
+
+
+if __name__ == '__main__':
+    main()
